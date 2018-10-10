276409733ab36073e734039e6799974a1f4214aa
BayanHammami <bayan.hammami@gmail.com>
2018-10-10 17:05:27 +1100

    change title

--- src/components/ResponsiveDrawer.js
+++ src/components/ResponsiveDrawer.js
@@ -93,7 +93,7 @@ class ResponsiveDrawer extends React.Component {
               <MenuIcon />
             </IconButton>
             <Typography variant="title" color="inherit" noWrap>
-              React Dashboard
+              React Dashboard Stage
             </Typography>
           </Toolbar>
         </AppBar>
